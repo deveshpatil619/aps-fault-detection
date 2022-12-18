3c38e9d7300a1991d984d6416c8d2b205936a5d9
deveshpatil619 <deveshpatil619@gmail.com>
2022-12-18 14:49:08 +0530

    updates

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
-## importing the modules
-import sys,os
-from sensor.logger import logging
+from sensor.config.mongo_db_connection import MongoDBClient
 from sensor.exception import SensorException
-from sensor.config.mo
-from sensor.utils import get_collections_as_dataframe
-from sensor.entity.config_entity import DataIngestionConfig
+from sensor.logger import logging
+import os,sys
 
 
+
+def test_exception():
+    try:
+        logging.info("We are dividing 1 by zero")
+        result = 2/0
+        print(result)
+    except Exception as e:
+        raise SensorException(e,sys)
 
 
 if __name__ == "__main__":
     try:
-        training_pipeline_config = config_entity.TrainingPipelineConfig()
-        data_ingestion_config = DataIngestionConfig(training_pipeline_config=training_pipeline_config)
-        print(data_ingestion_config.to_dict())
-
+        test_exception()
     except Exception as e:
         print(e)
 
+
+
+
+
+   # mongodb_client = MongoDBClient()
+    #print(mongodb_client.database.list_collection_names())
 
