c802c5b60a09e439c0384b93d0d9afa2387a0e30
DEVESH PATIL <deveshpatil619@gmail.com>
2022-12-25 16:14:00 +0530

    updates

---
+++ sensor/ml/model/estimator.py
@@ -0,0 +1,12 @@
+import os,sys
+class TargetValueMapping:    ## for the target column values that we need to map
+    def __init__(self) :
+        self.neg : int = 0
+        self.pos : int = 1
+
+    def to_dict(self):
+        return self.__dict__
+
+    def reverse_mapping(self):
+        mapping_response = self.to_dict()
+        return dict(zip(mapping_response.values(),mapping_response.keys()))
--- sensor/utils/main_utils.py
+++ sensor/utils/main_utils.py
@@ -1,8 +1,10 @@
 import yaml
-import numpy as np
-import os,sys
 from sensor.exception import SensorException
 from sensor.logger import logging
+import os,sys
+import numpy as np
+
+
 
 
 """ Reading the yaml_file"""
@@ -14,7 +16,7 @@ def read_yaml_file(file_path: str) -> dict:
         raise SensorException(e, sys) from e
 
     
-""" Reading the yaml_file and creating if we want the yaml_file if replace we keep to true"""
+""" writing into the yaml_file and creating if we want the yaml_file if replace we keep to true"""
 
 def write_yaml_file(file_path:str,content:object,replace:bool = False) ->None:
     try:
@@ -33,10 +35,67 @@ def write_yaml_file(file_path:str,content:object,replace:bool = False) ->None:
 
 
 
+""" saving the numpy array data """
+
+def save_numpy_array_data(file_path: str, array: np.array):
+    """
+    Save numpy array data to file
+    file_path: str location of file to save
+    array: np.array data to save
+    """
+
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path,"wb") as numpy_file:
+            np.save(numpy_file,array)
+
+    except Exception as e:
+        raise SensorException(e,sys) from e
 
 
 
+""" loading the numpy array data """
 
+def load_numpy_array_data(file_path:str)->np.array:
+    """
+    load numpy array data from file
+    file_path: str location of file to load
+    return: np.array data loaded
+    """
+
+    try:
+        with open(file_path,"rb") as numpy_file:
+            return np.load(numpy_file)
+    except Exception as e:
+        raise SensorException(e,sys) from e
+
+
+""" using the dill to save the object file"""
+def save_object(file_path:str,obj:object)->None:
+    try:
+        logging.info("Entered the save_object method of MainUtils class")
+
+        os.makedirs(os.path.dirname(file_path),exist_ok=True) ## making the folder 
+        with open(file_path,"wb") as file_obj:
+            dill.dump(obj,file_obj)
+
+        logging.info("Exited the save_object method of MainUtils class")
+    except Exception as e:
+        raise SensorException(e,sys) from e
+
+
+""" loading the object file"""
+
+def load_object(file_path:str)->object:
+    try:
+        if not os.path.exists(file_path):
+            raise Exception (f"The file: {file_path} is not exists")
+        with open(file_path,"rb") as file_obj:
+            return dill.load(file_obj)
+    
+    except Exception as e:
+        raise SensorException(e,sys) from e
 
 
 
