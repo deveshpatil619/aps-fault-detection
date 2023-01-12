24c458103dab2325fd1513a5191a650d050396cf
DEVESH PATIL <deveshpatil619@gmail.com>
2023-01-12 10:52:28 +0530

    updates

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ async def predict_route():
         y_pred = model.predict(df)
         df['predicted_column'] = y_pred
         df['predicted_column'].replace(TargetValueMapping().reverse_mapping(),inplace=True)
-        
+        return df.to_html()
         #decide how to return file to user.
         
     except Exception as e:
